f70a0ed55a90c9a33f6fcb3f732959ef5a22ea19
Вадим <vadim.ppp@mail.ru>
2021-07-24 19:48:24 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dgvViewer = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Имя = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Скидка = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Зависимость = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Описание = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Цена = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ЦенаСоСкидкой = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Создание каталога";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(61, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(38, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Создать таблицу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(401, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Добавить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(522, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(315, 148);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(522, 154);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(316, 197);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 8;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(522, 197);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Скидка";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(237, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Зависимость";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(459, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Описание";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(471, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Цена";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(507, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "ЦенаСУчетомСкидки";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Миасс",
+            "Амелия",
+            "Тест1",
+            "Тест2",
+            "Курган"});
+            this.comboBox1.Location = new System.Drawing.Point(300, 93);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dgvViewer
+            // 
+            this.dgvViewer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvViewer.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Имя,
+            this.Скидка,
+            this.Зависимость,
+            this.Описание,
+            this.Цена,
+            this.ЦенаСоСкидкой});
+            this.dgvViewer.Location = new System.Drawing.Point(12, 260);
+            this.dgvViewer.Name = "dgvViewer";
+            this.dgvViewer.Size = new System.Drawing.Size(744, 150);
+            this.dgvViewer.TabIndex = 17;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            // 
+            // Имя
+            // 
+            this.Имя.HeaderText = "Имя";
+            this.Имя.Name = "Имя";
+            // 
+            // Скидка
+            // 
+            this.Скидка.HeaderText = "Скидка";
+            this.Скидка.Name = "Скидка";
+            // 
+            // Зависимость
+            // 
+            this.Зависимость.HeaderText = "Зависимость";
+            this.Зависимость.Name = "Зависимость";
+            // 
+            // Описание
+            // 
+            this.Описание.HeaderText = "Описание";
+            this.Описание.Name = "Описание";
+            // 
+            // Цена
+            // 
+            this.Цена.HeaderText = "Цена";
+            this.Цена.Name = "Цена";
+            // 
+            // ЦенаСоСкидкой
+            // 
+            this.ЦенаСоСкидкой.HeaderText = "ЦенаСоСкидкой";
+            this.ЦенаСоСкидкой.Name = "ЦенаСоСкидкой";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(681, 52);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 23);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Загрузить данные";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(681, 119);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(119, 23);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "Удалить все записи";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(681, 167);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(119, 64);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "Рассчитать Цену С Учетом Скидки";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 422);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dgvViewer);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvViewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dgvViewer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Имя;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Скидка;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Зависимость;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Описание;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Цена;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ЦенаСоСкидкой;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            m_dbConn = new SQLiteConnection();
+            m_sqlCmd = new SQLiteCommand();
+        }
+        private String dbFileName;
+        private SQLiteConnection m_dbConn;
+        private SQLiteCommand m_sqlCmd;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dbFileName = textBox1.Text + ".sqlite";
+            if (!File.Exists(dbFileName)) SQLiteConnection.CreateFile(dbFileName);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                m_dbConn = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                m_dbConn.Open();
+                m_sqlCmd.Connection = m_dbConn;
+
+                m_sqlCmd.CommandText = "CREATE TABLE IF NOT EXISTS Catalog (id INTEGER PRIMARY KEY AUTOINCREMENT, name Text, discount Text,dependence Text,description Text,price Text, proverka Text)";
+                m_sqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Таблица успешно создана!");
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (m_dbConn.State != ConnectionState.Open)
+            {
+                dbFileName = "catalog1.sqlite";
+                m_dbConn = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                m_dbConn.Open();
+                m_sqlCmd.Connection = m_dbConn;
+            }
+
+            try
+            {
+                m_sqlCmd.CommandText = "INSERT INTO Catalog ('name', 'discount','dependence','description','price','proverka') values ('" + comboBox1.Text + "' , '" + textBox4.Text + "', '" + textBox6.Text + "','" + textBox3.Text + "','" + textBox5.Text + "','" + textBox7.Text + "')";
+                m_sqlCmd.ExecuteNonQuery();
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            m_dbConn.Close();
+
+            comboBox1.SelectedIndex = -1;
+            textBox3.Text = "";
+            textBox3.Clear();
+            textBox4.Text = "";
+            textBox4.Clear();
+            textBox5.Text = "";
+            textBox5.Clear();
+            textBox6.Text = "";
+            textBox6.Clear();
+            textBox7.Text = "";
+            textBox7.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DataTable dTable = new DataTable();
+            String sqlQuery;
+
+            if (m_dbConn.State != ConnectionState.Open)
+            {
+                dbFileName = "catalog1.sqlite";
+                m_dbConn = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                m_dbConn.Open();
+            }
+
+            try
+            {
+                sqlQuery = "SELECT * FROM Catalog";
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(sqlQuery, m_dbConn);
+                adapter.Fill(dTable);
+
+                if (dTable.Rows.Count > 0)
+                {
+                    dgvViewer.Rows.Clear();
+
+                    for (int i = 0; i < dTable.Rows.Count; i++)
+                        dgvViewer.Rows.Add(dTable.Rows[i].ItemArray);
+                }
+                else
+                    MessageBox.Show("Database is empty");
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            m_dbConn.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dgvViewer.Rows.Clear();
+            dgvViewer.Refresh();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        {
+                            textBox4.Text = "4";
+                            textBox4.Enabled = false; // чтобы не был активным
+                            textBox6.Text = "Нет";
+                            textBox6.Enabled = false; // чтобы не был активным
+                            textBox3.Text = "Test1";
+                            textBox3.Enabled = false; // чтобы не был активным
+                            textBox5.Text = "57470";
+                            textBox5.Enabled = true; // чтобы  был активным
+                        }
+                        break;
+
+                    case 1:
+                        {
+                            textBox4.Text = "5";
+                            textBox4.Enabled = false; // чтобы не был активным
+                            textBox6.Text = "Есть";
+                            textBox6.Enabled = false; // чтобы не был активным
+                            textBox3.Text = "Test55";
+                            textBox3.Enabled = false; // чтобы не был активным
+                            textBox5.Text = "5360";
+                            textBox5.Enabled = true; // чтобы  был активным
+                        }
+                        break;
+
+                    case 2:
+                        {
+                            textBox4.Text = "2";
+                            textBox4.Enabled = false; // чтобы не был активным
+                            textBox6.Text = "Есть";
+                            textBox6.Enabled = false; // чтобы не был активным
+                            textBox3.Text = "Test88";
+                            textBox3.Enabled = false; // чтобы не был активным
+                            textBox5.Text = "136540";
+                            textBox5.Enabled = true; // чтобы  был активным
+                        }
+                        break;
+                    case 3:
+                        {
+                            textBox4.Text = "0";
+                            textBox4.Enabled = false; // чтобы не был активным
+                            textBox6.Text = "Есть";
+                            textBox6.Enabled = false; // чтобы не был активным
+                            textBox3.Text = "Test66";
+                            textBox3.Enabled = false; // чтобы не был активным
+                            textBox5.Text = "54054";
+                            textBox5.Enabled = true; // чтобы  был активным
+                        }
+                        break;
+                    case 4:
+                        {
+                            textBox4.Text = "11";
+                            textBox4.Enabled = false; // чтобы не был активным
+                            textBox6.Text = "Нет";
+                            textBox6.Enabled = false; // чтобы не был активным
+                            textBox3.Text = "Test11";
+                            textBox3.Enabled = false; // чтобы не был активным
+                            textBox5.Text = "57850";
+                            textBox5.Enabled = true; // чтобы  был активным
+                        }
+                        break;
+
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double a;
+            double b;
+            double c;
+            a = Convert.ToDouble(textBox4.Text);
+            b = Convert.ToDouble(textBox5.Text);
+            c = b - ((b * a) / 100);
+            textBox7.Text = Convert.ToString(c);
+        }
+    }
+}
